0ee4ba58af0c5e3032adbf719cac4b88295256ec
Anneeie <ani.khachatryan2609@gmail.com>
2024-05-29 23:46:49 +0400

    final commit

--- src/project/train/train.py
+++ src/project/train/train.py
@@ -7,7 +7,10 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.svm import SVR
 from sklearn import metrics
-
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.callbacks import EarlyStopping
+from sklearn.preprocessing import StandardScaler
 
 
 data = pd.read_csv("C:\\Users\\user\PycharmProjects\\final_project\src\project\data\data.csv")
@@ -57,3 +60,46 @@ mape = np.mean(np.abs((y_test - y_test_pred) / np.abs(y_test)))
 print('Mean Absolute Percentage Error (MAPE):', round(mape , 2))
 print('R-Squared:', metrics.r2_score(y_test, y_test_pred))
 print('Accuracy:', round((1 - mape), 2))
+
+
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = Sequential()
+
+model.add(Dense(256, input_dim=X_train_scaled.shape[1], activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+model.fit(X_train_scaled, y_train, epochs=50, batch_size=64, validation_split=0.2, callbacks=[early_stopping])
+
+loss = model.evaluate(X_test_scaled, y_test)
+print('Test loss:', loss)
+
+y_pred = model.predict(X_test_scaled)
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'MSE: {mse}')
+print(f'MAE: {mae}')
+print(f'R^2 Score: {r2}')
